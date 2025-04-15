fd3a1f2cc64333a8f9d645df614716e8afec2ada
TrailingWhitespace <flash1719@protonmail.com>
2025-04-15 17:44:03 +0000

    some text here and there

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 function App() {
   return (
     <>
-      <h1>Test</h1>
+      <img class="avatar" src="src/img1.png" />
+      <div class="main">
+        <p class="heading1">Heya 👋 ! I'm</p>
+        <h1 class="heading2">Prabhas Gunda.</h1>
+        <p class="heading3">
+          A first-year computer science student that likes
+          <br></br>
+          <a href="#" class="Underline">
+            Neovim
+          </a>
+          , <a class="Underline">Python</a> and anything tech.<br></br>
+          <br></br>I've started to lean more towards web development as of late
+          but I try to maintain a balance between the things I do.
+        </p>
+      </div>
     </>
   );
 }
